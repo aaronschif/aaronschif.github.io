b7ac42d0d1a405cb25675de7f06f920bfc36fd9c
Aaron Schif <aaronschif@gmail.com>
2016-02-10 22:24:32 -0600

    Allow static deploy

--- builder.py
+++ builder.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 from webassets import Environment as AssetsEnvironment
 from os.path import abspath, dirname
 
 import cherrypy
 
-BASE_PATH = dirname(abspath(__file__))
+BASE_PATH = Path(__file__).parent
 
 env = Environment(
     loader=FileSystemLoader('templates'),
@@ -13,34 +14,48 @@ env = Environment(
         'webassets.ext.jinja2.AssetsExtension'
     ],
     cache_size=0
-    )
+)
 
 assets_env = AssetsEnvironment(
     debug=True,
     url='/static',
-    directory=BASE_PATH+'/var/static/',
-    load_path=[BASE_PATH+'/static/']
+    directory=str(BASE_PATH/'var/static/'),
+    load_path=[str(BASE_PATH/'static/')]
 )
 env.assets_environment = assets_env
 
+assets_env.register(
+    'css',
+    'main.sass',
+    output='main.css',
+    filters='libsass',
+)
 
-class Router(object):
-    @cherrypy.expose
-    def index(self):
-        template = env.get_template('home.jade')
-        template = env.get_template('resume.jade')
-        return template.render()
-
-    @cherrypy.expose
-    def resume(self):
-        template = env.get_template('resume.jade')
-        return template.render()
 
 if __name__ == '__main__':
-    conf = {
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': BASE_PATH+'/var/static/'
-        }
-    }
-    cherrypy.quickstart(Router(), '/', conf)
+    BASE_PATH.joinpath('var/index.html').write_text(env.get_template('home.jade').render())
+    BASE_PATH.joinpath('var/resume.html').write_text(env.get_template('resume.jade').render())
+
+
+
+
+# class Router(object):
+#     @cherrypy.expose
+#     def index(self):
+#         template = env.get_template('home.jade')
+#         template = env.get_template('resume.jade')
+#         return template.render()
+#
+#     @cherrypy.expose
+#     def resume(self):
+#         template = env.get_template('resume.jade')
+#         return template.render()
+#
+# if __name__ == '__main__':
+#     conf = {
+#         '/static': {
+#             'tools.staticdir.on': True,
+#             'tools.staticdir.dir': BASE_PATH+'/var/static/'
+#         }
+#     }
+#     cherrypy.quickstart(Router(), '/', conf)
