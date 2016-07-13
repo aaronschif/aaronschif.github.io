bba42c63c49fda9d6e76b2e2644704da3a306c96
Aaron Schif <aschif@netdevgroup.com>
2016-07-13 09:03:23 -0500

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var svg2png = require('gulp-svg2png');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/*.sass')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('image', function () {
+    return gulp.src('src/*.svg')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/'))
+        .pipe(svg2png())
+        .pipe(gulp.dest('public/'));
+})
+
+gulp.task('jade', function () {
+  return gulp.src('src/*.jade')
+  .pipe(pug({
+    pretty: true,
+  })).pipe(gulp.dest('public/'));
+});
+
+gulp.task('default', ['sass', 'jade', 'image']);
