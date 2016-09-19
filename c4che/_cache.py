75a1ecfb8ca44520d1fceedcd99a34346ce43ab5
Aaron Schif <aschif@netdevgroup.com>
2016-09-18 21:54:29 -0600

    Update documentation

--- c4che/_cache.py
+++
@@ -1,5 +0,0 @@
-BINDIR = '/usr/local/bin'
-LIBDIR = '/usr/local/lib'
-PREFIX = '/usr/local'
-PYJADE = ['/home/aaron/Projects/aaronschif.github.io/.python-venv/bin/pyjade']
-SASSC = ['/home/aaron/Projects/aaronschif.github.io/.python-venv/bin/sassc']
